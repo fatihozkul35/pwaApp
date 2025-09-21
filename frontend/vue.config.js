eabfbda896cca4667a44d8d0d8183450409674b4
fatihozkul35 <fatihozkul3541@gmail.com>
2025-09-21 17:38:27 +0200

    fix mainfest

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,9 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   pwa: {
     name: 'PWA App',
+    shortName: 'PWA',
     themeColor: '#4DBA87',
+    backgroundColor: '#ffffff',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
@@ -13,14 +16,68 @@ module.exports = defineConfig({
       swSrc: './src/service-worker.js',
     },
     iconPaths: {
-      favicon32: null,
-      favicon16: null,
-      appleTouchIcon: null,
-      maskIcon: null,
-      msTileImage: null
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
     },
     manifestOptions: {
-      icons: []
+      background_color: '#ffffff',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+      orientation: 'portrait-primary',
+      icons: [
+        {
+          src: 'img/icons/icon-72x72.png',
+          sizes: '72x72',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: 'img/icons/icon-96x96.png',
+          sizes: '96x96',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: 'img/icons/icon-128x128.png',
+          sizes: '128x128',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: 'img/icons/icon-144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: 'img/icons/icon-152x152.png',
+          sizes: '152x152',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: 'img/icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: 'img/icons/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: 'img/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable any'
+        }
+      ]
     }
   },
   devServer: {
